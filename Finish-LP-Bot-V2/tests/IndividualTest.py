7793e64f593053cdada2f204beb4ea304a7757c3
JoviOakz <joao.mateussdc@gmail.com>
2025-01-17 09:09:49 -0300

    feat: new version of Bot created

---
+++ Finish-LP-Bot-V2/tests/IndividualTest.py
@@ -0,0 +1,25 @@
+import pyautogui as bot
+
+bot.FAILSAFE = True
+bot.PAUSE = 0.35
+
+bot.moveTo(606, 848)
+bot.mouseDown()
+bot.moveTo(846, 848, duration=0.5)
+bot.mouseUp()
+bot.sleep(0.3)
+
+try:
+    check_box = list(bot.locateAllOnScreen('../images/CHECK.png', grayscale=True, confidence=0.8))
+    if check_box:
+        try:
+            have_baixa = list(bot.locateAllOnScreen('../images/BAIXACONF.png', grayscale=True, confidence=0.8))
+            if have_baixa:
+                if len(check_box) == len(have_baixa):
+                    print('Deu boa')
+                else:
+                    print('Deu merda')
+        except Exception as e:
+            print(f'Erro: {e}')
+except Exception as e:
+    print(f'Erro: {e}')
