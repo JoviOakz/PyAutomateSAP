7916721c05ab86729ed8e5b1010350edaf230750
JoviOakz <joao.mateussdc@gmail.com>
2025-01-27 16:03:45 -0300

    PDF reader created

--- PDF-Reader/Reader.py
+++ PDF-Reader/Reader.py
@@ -1,14 +1,23 @@
-import pytesseract
 from pdf2image import convert_from_path
-from PIL import Image
+import pytesseract
 
-path = "S:/TEC/Technical_Function/2.2.Toolshop_Devices/03.Shared/01.Toolshop/03_Inspeção/FERRAMENTARIA/10 - Hallyessa/Nina/3 - Projeto Encerramento LPs/LPs/2024/KW 39/Scanned_from_a_Lexmark_Multifunction_Product31-10-2024-152038.pdf"
-pytesseract.pytesseract.tesseract_cmd = r'C:\\Users\\cun1ct\\AppData\\Local\\Programs\\Python\\Python312\\Lib\\site-packages\\pytesseract'
-
-pages = convert_from_path(path, poppler_path="C:/Users/cun1ct/Desktop/Release-24.08.0-0/poppler-24.08.0/Library/bin")
-text_data = []
-
-for page in pages:
-    text = pytesseract.image_to_string(page)
-    text_data.append(text)
-print(text_data)
+def extract_text_from_pdf(pdf_path):
+    images = convert_from_path(pdf_path)
+ 
+    extracted_text = ""
+    for i, image in enumerate(images):
+        rotated_image = image.rotate(-90, expand=True)
+ 
+        text = pytesseract.image_to_string(rotated_image)
+ 
+        index0 = text.index("LP-")
+        index1 = text.index("\n", index0+1)
+ 
+        lp = text[index0:index1]
+ 
+        extracted_text += f"\n--- Page {i+1} ---\n{lp}"
+    return extracted_text
+ 
+pdf_path = "PDF-Reader/test.pdf"
+text = extract_text_from_pdf(pdf_path)
+print(text)
