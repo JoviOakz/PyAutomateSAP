88e2299b5241102e5607893d8e375cbd37a639a5
JoviOakz <joao.mateussdc@gmail.com>
2025-01-27 17:18:20 -0300

    fix: no encounter LPs resolved

--- PDF-Reader/Reader.py
+++ PDF-Reader/Reader.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 from pdf2image import convert_from_path
 import pytesseract
@@ -7,18 +8,22 @@ def extract_text_from_pdf(pdf_path):
  
     extracted_text = []
     for i, image in enumerate(images):
-        rotated_image = image.rotate(-90, expand=True)
+        try:
+            rotated_image = image.rotate(90, expand=True)
  
-        text = pytesseract.image_to_string(rotated_image)
- 
-        index0 = text.index("LP-")
-        index1 = text.index("\n", index0+1)
- 
-        lp = text[index0:index1]
-        extracted_text.append(lp.strip())
+            text = pytesseract.image_to_string(rotated_image)
+    
+            index0 = text.index("LP-")
+            index1 = text.index("\n", index0+1)
+    
+            lp = text[index0:index1]
+            extracted_text.append(lp.strip())
+        except:
+            print("Error on page ", i+1)
+
     return extracted_text
  
-pdf_path = "PDF-Reader/test.pdf"
+pdf_path = "PDF-Reader/LPs.pdf"
 text = extract_text_from_pdf(pdf_path)
 print(text)
 
