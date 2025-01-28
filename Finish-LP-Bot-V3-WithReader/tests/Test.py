b2c31116e16c850ae34532aee128df922b512509
JoviOakz <joao.mateussdc@gmail.com>
2025-01-28 11:03:17 -0300

    feat: added new tests for PDF Reader

--- Finish-LP-Bot-V3-WithReader/tests/Test.py
+++ Finish-LP-Bot-V3-WithReader/tests/Test.py
@@ -12,13 +12,3 @@ df = pd.read_excel(pdf_path, engine='openpyxl')
 
 lp_value = df.at[0, 'LP']
 pyperclip.copy(lp_value)
-
-bot.press('win')
-bot.typewrite('bloco de notas')
-bot.press('enter')
-bot.sleep(0.3)
-bot.moveTo(x=500, y=500, duration=0.5)
-bot.click()
-bot.hotkey('ctrl', 'v')
-bot.press('enter')
-bot.write('deu certo')
