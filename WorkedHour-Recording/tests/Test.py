1324579e77c62faaf0b7df632f39ca002d895ccd
JoviOakz <joao.mateussdc@gmail.com>
2025-02-20 15:57:37 -0300

    fix

--- WorkedHour-Recording/tests/Test.py
+++ WorkedHour-Recording/tests/Test.py
@@ -1,18 +1,62 @@
 import pyautogui as bot
 import pandas as pd
-from openpyxl.styles import PatternFill
 
 bot.FAILSAFE = True
 bot.PAUSE = 0.25
 
-excel_path = "../ApontamentoYesica.xlsx"
+bot.click(1802, 14)
+
+line = 18
+
+excel_path = "../ApontamentoYesica.xlsm"
 df = pd.read_excel(excel_path, engine='openpyxl')
 
-df.at[11, 'Elemento PEP'] = PatternFill(start_color="00FF00", end_color="00FF00", fill_type="solid")
+def press_key(key, times):
+    for _ in range(times):
+        if key == 'ctrls':
+            bot.hotkey('ctrl', 's')
+        else:
+            bot.press(key)
 
-# bot.typewrite('Planejadora Yesica - 20/02/2025')
-# bot.typewrite('H')
-# bot.typewrite('100')
-# bot.typewrite('025PROJ')
+for _ in range(82):
+    lp = df.at[line, 'Elemento PEP']
 
-# bot.typewrite('92886895')
+    bot.typewrite(str(lp))
+
+    press_key('f7', 1)
+
+    bot.sleep(3)
+
+    press_key('tab', 2)
+    bot.typewrite('Planejadora Yesica - 20.02.2025')
+    press_key('tab', 2)
+    bot.typewrite('H')
+    press_key('tab', 2)
+    bot.typewrite('FCTLIY')
+    press_key('tab', 2)
+    bot.typewrite('100')
+    press_key('tab', 1)
+    bot.typewrite('025PROJ')
+    press_key('ctrls', 1)
+
+    bot.sleep(2.5)
+
+    press_key('tab', 1)
+    press_key('enter', 1)
+
+    bot.sleep(1)
+    
+    bot.typewrite('92886895')
+    press_key('enter', 1)
+
+    bot.sleep(1)
+    
+    press_key('tab', 1)
+
+    bot.sleep(0.3)
+    
+    press_key('enter', 1)
+    
+    bot.sleep(1)
+
+    line += 1
