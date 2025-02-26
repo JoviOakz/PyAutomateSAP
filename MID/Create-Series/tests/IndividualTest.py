bbbdb76af16de213afa7259512aa0e9dae39820c
JoviOakz <joao.mateussdc@gmail.com>
2025-02-26 10:56:40 -0300

    fix: bot working

--- MID/Create-Series/tests/IndividualTest.py
+++ MID/Create-Series/tests/IndividualTest.py
@@ -1,7 +1,17 @@
 import pyautogui as bot
+import pandas as pd
 
 bot.FAILSAFE = True
 bot.PAUSE = 0.25
 
-bot.sleep(1)
-bot.hotkey('ctrl', 's')
+excel_path = "../Data.xlsx"
+df = pd.read_excel(excel_path, engine='openpyxl')
+
+line = 0
+
+norm = df.at[line, 'Norma']
+
+if norm == 4718301460:
+    print('Boa!')
+else:
+    print('Não deu!')
