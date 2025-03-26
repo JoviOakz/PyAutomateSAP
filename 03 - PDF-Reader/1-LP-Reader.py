853c97860ef57421690b4a92b749cabc4968da86
JoviOakz <joao.mateussdc@gmail.com>
2025-03-26 16:45:37 -0300

    fix: PDF reader have new comments

--- 03 - PDF-Reader/1-LP-Reader.py
+++ 03 - PDF-Reader/1-LP-Reader.py
@@ -3,16 +3,18 @@ from pdf2image import convert_from_path
 import pytesseract
 import re
 
+# REMOVE ALL EXTRA CHARACTERS
 def limpar_string(s):     
       return re.sub(r'[^LP0-9-]', '', s)
 
+# CATCH ALL LP's IN PDF
 def extract_text_from_pdf(pdf_path):
     images = convert_from_path(pdf_path)
  
     extracted_text = []
     for i, image in enumerate(images):
         try:
-            # CHANGE ROTATION
+            # CHANGE THE PDF ROTATION
             rotated_image = image.rotate(90, expand=True)
  
             text = pytesseract.image_to_string(rotated_image)
@@ -43,18 +45,18 @@ def extract_text_from_pdf(pdf_path):
 
     return extracted_text
 
-# CHANGE PDF PATH
+# PDF PATH
 pdf_path = "03 - PDF-Reader/LPs - KW06 - deitado.pdf"
+
+# FILTER LP's CORRECTLY
 text = extract_text_from_pdf(pdf_path)
 text = [limpar_string(s) for s in text]
-
 text = [
     t[:9] if len(t) > 9 else t[:3] + "0" + t[3:] if len(t) == 8 else t
     for t in text
 ]
-
 print(text)
 
+# EXTRACT TO EXCEL
 df = pd.DataFrame({"LP": text, "Status": ""})
-
 df.to_excel("Open-LPs.xlsx")
--- 03 - PDF-Reader/2-OM-Reader.py
+++ 03 - PDF-Reader/2-OM-Reader.py
@@ -3,16 +3,18 @@ from pdf2image import convert_from_path
 import pytesseract
 import re
  
+# REMOVE ALL EXTRA CHARACTERS
 def limpar_string(s):    
       return re.sub(r'[^0-9]', '', s)
  
+# CATCH ALL OM's IN PDF
 def extract_text_from_pdf(pdf_path):
     images = convert_from_path(pdf_path)
  
     extracted_text = []
     for i, image in enumerate(images):
         try:
-            # CHANGE ROTATION
+            # CHANGE THE PDF ROTATION
             rotated_image = image.rotate(0, expand=True)
  
             text = pytesseract.image_to_string(rotated_image)
@@ -47,12 +49,14 @@ def extract_text_from_pdf(pdf_path):
  
     return extracted_text
  
-# CHANGE PDF PATH
+# PDF PATH
 pdf_path = "PDF-Reader/OMs - KW07.pdf"
+
+# FILTER OM's CORRECTLY
 text = extract_text_from_pdf(pdf_path)
 text = [limpar_string(s) for s in text]
 print(text)
  
+# EXTRACT TO EXCEL
 df = pd.DataFrame({"OM": text, "Status": ""})
- 
 df.to_excel("Open-OMs.xlsx")
