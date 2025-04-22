a9c1abeeb64f734f4210c5bdb8a39ed5ba3e3564
JoviOakz <joao.mateussdc@gmail.com>
2025-04-22 13:23:54 -0300

    fix

--- 02 - Finish-OM/Bot.py
+++ 02 - Finish-OM/Bot.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 # SOFTWARE GLOBAL SETTINGS
 bot.FAILSAFE = True
-bot.PAUSE = 1
+bot.PAUSE = 0.5
 
 # PUT DOWN THE CODE SCREEN
 bot.click(1802, 14)
@@ -40,7 +40,7 @@ def open_om():
 # VERIFY IF THE ENCE STATUS ALREADY EXISTS
 def ence_exist():
     try:
-        have_ence = bot.locateOnScreen('images/ENCE.png', grayscale=True, confidence=0.9)
+        have_ence = bot.locateOnScreen('images/ENCE.png', grayscale=True)
 
         if have_ence:
             df.at[line, 'Status'] = 'Encerrado!'
@@ -74,7 +74,7 @@ def com_complete():
     bot.sleep(1.5)
 
     try:
-        conclude = bot.locateAllOnScreen('images/CONCLUIR.png', grayscale=True, confidence=0.9)
+        conclude = bot.locateOnScreen('images/CONCLUIR.png', grayscale=True)
         
         if conclude:
             df.at[line, 'Status'] = 'Encerrado!'
@@ -88,7 +88,7 @@ def com_complete():
         print('Warning not found!')
 
     try:
-        error = bot.locateAllOnScreen('images/ERROR.png', grayscale=True, confidence=0.9)
+        error = bot.locateOnScreen('images/ERROR.png', grayscale=True)
         
         if error:
             df.at[line, 'Status'] = 'Ordem pendente!'
@@ -112,7 +112,7 @@ def com_complete():
 
 # EXCEL CONFIG
 om_qty = 16
-line = 1
+line = 8
 
 # REPEAT QUANTITY TO PROGRAM RUN
 repeat_qty = om_qty - line
@@ -126,7 +126,7 @@ for _ in range(repeat_qty):
 
     if jump_process != 1:
         try:
-            tec_finished = bot.locateAllOnScreen('images/BANDEIRA.png', grayscale=True, confidence=0.9)
+            tec_finished = bot.locateOnScreen('images/BANDEIRA.png', grayscale=True)
             
             if tec_finished:
                 tec_complete()
