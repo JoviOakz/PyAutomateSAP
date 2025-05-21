2b9bd24baee57480992d1cc358e86e41b8f9e6f3
JoviOakz <joao.mateussdc@gmail.com>
2025-05-21 17:16:26 -0300

    added new tests

--- teste.py
+++ teste.py
@@ -1,7 +1,9 @@
 import pyautogui
 
-# pyautogui.alert('Dá um OK aí!')
-# print(pyautogui.prompt('Escreva algo'))
+# pyautogui.alert(text='Fala gurias eu sou o Ribeiro!', title='Caixa de texto!', button='OK')
+# pyautogui.confirm(text='Fala gurias eu sou o Ribeiro!', title='Caixa de texto!', buttons=['SIM', 'OR NOT?!'])
+# print(pyautogui.prompt(text='Diga a sua cor favorita', title='Caixa de pergunta', default='Hobs'))
+# print(pyautogui.password(text='Diga a sua senha favorita', title='Caixa de phishing', default='123', mask='*'))
 
 #! python3
 # import pyautogui, sys
@@ -21,10 +23,10 @@ import pyautogui
 # pyautogui.moveTo(100, 100, 2, pyautogui.easeInQuad)
 
 
-pyautogui.sleep(3)
+# pyautogui.sleep(3)
 
 # for _ in range(100):
 #     pyautogui.click()
 
-pyautogui.click(clicks=10000, interval=0.00001)
+# pyautogui.click(clicks=10000, interval=0.00001)
 # pyautogui.click(clicks=10000)
