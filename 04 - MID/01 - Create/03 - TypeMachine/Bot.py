bec3143e91bbf84660e37fd3e7ca0212ffdf58d6
JoviOakz <joao.mateussdc@gmail.com>
2025-06-09 16:05:49 -0300

    fix: merge typeMachine bot's

--- 04 - MID/01 - Create/03 - TypeMachine/Bot.py
+++ 04 - MID/01 - Create/03 - TypeMachine/Bot.py
@@ -1,15 +1,18 @@
 import pyautogui as bot
 import pandas as pd
+import pytesseract
+import cv2
+import numpy as np
+from PIL import Image
+import difflib
 
 bot.FAILSAFE = True
-bot.PAUSE = 1
+bot.PAUSE = 0.35
 
 bot.click(1802, 14)
 
 excel_path = "Data.xlsx"
 df = pd.read_excel(excel_path, engine='openpyxl')
-
-part_number_coords = (596, 850, 454, 242)
 
 def press_key(key, times):
     for _ in range(times):
@@ -30,6 +33,7 @@ def register_verification():
             press_key('enter', 1)
             bot.sleep(0.3)
             bot.typewrite(str(part_number))
+
             bot.sleep(0.3)
             bot.click(1136, 1176)
             
@@ -38,35 +42,133 @@ def register_verification():
     except Exception:
         return False
 
-part_number_qty = 772
-line = 310
+def find_position(part_number):
+    part_number = str(part_number).upper().replace('O', '0').replace(' ', '')
+
+    screenshot = bot.screenshot(region=(600, 852, 542, 236))
+    img_np = np.array(screenshot)
+
+    gray = cv2.cvtColor(img_np, cv2.COLOR_RGB2GRAY)
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    enhanced = clahe.apply(gray)
+    _, thresh = cv2.threshold(enhanced, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    upscale = cv2.resize(thresh, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+
+    final_image = Image.fromarray(upscale)
+
+    custom_config = r'--oem 3 --psm 7 -c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    part_numbers_text = pytesseract.image_to_string(final_image, config=custom_config)
+
+    part_numbers_list = [
+        line.strip().upper().replace('O', '0').replace(' ', '')
+        for line in part_numbers_text.splitlines() 
+        if line.strip()
+    ]
+
+    print(part_numbers_list)
+
+    if part_number in part_numbers_list:
+        return part_numbers_list.index(part_number) + 1
+
+    for idx, item in enumerate(part_numbers_list):
+        ratio = difflib.SequenceMatcher(None, item, part_number).ratio()
+        
+        if ratio >= 0.96:
+            print(f"Parecido com {part_number}: {item} (similaridade: {ratio:.2f})")
+            return idx + 1
+
+    return 0
+
+part_number_qty = 2061
+line = 0
 
 repeat_count = part_number_qty - line
 
 for _ in range(repeat_count):
     need_register = False
 
-    part_number = df.at[line, 'X'] # W -> 764 | X -> 772 | S -> 775 | K -> 29
+    part_number = df.at[line, 'Bico (normal final)'] # W -> 764 | X -> 772 | S -> 775 | K -> 29 | Bico (normal final) -> 2061
+
+    if str(part_number).startswith('433'):
+        part_number = ('0' + str(part_number))
 
     bot.click(812, 830)
     bot.sleep(0.3)
 
     bot.typewrite(str(part_number))
-    
-    bot.sleep(1.5)
+
+    bot.sleep(1.25)
 
     need_register = register_verification()
 
     if not need_register:
-        bot.click(844, 872)
-        bot.sleep(0.3)
-        press_key('tab', 1)
-        bot.sleep(0.3)
-        bot.typewrite('01')
-        bot.sleep(0.3)
-        bot.click(1614, 824)
-        bot.sleep(1)
+        click_coords = {
+            1: (874, 870),
+            2: (874, 922),
+            3: (874, 970),
+            4: (874, 1020),
+            5: (874, 1070)
+        }
+
+        bot.moveTo(964, 920)
+        bot.scroll(1000)
+        bot.moveTo(400, 720)
+
+        bot.sleep(0.5)
+
+        posicao = find_position(part_number)
+
+        if posicao in click_coords:
+            x, y = click_coords[posicao]
+            bot.click(x, y)
+            press_key('tab', 1)
+            bot.typewrite('01')
+            press_key('tab', 2)
+            press_key('space', 1)
+        else:
+            bot.moveTo(964, 920)
+            bot.scroll(-290)
+            bot.moveTo(400, 720)
+            
+            bot.sleep(0.5)
+
+            posicao = find_position(part_number)
+
+            if posicao in click_coords:
+                x, y = click_coords[posicao]
+                bot.click(x, y)
+                press_key('tab', 1)
+                bot.typewrite('01')
+                press_key('tab', 2)
+                press_key('space', 1)
+            else:
+                bot.moveTo(964, 920)
+                bot.scroll(-290)
+                bot.moveTo(400, 720)
+
+                posicao = find_position(part_number)
+
+                if posicao in click_coords:
+                    x, y = click_coords[posicao]
+                    bot.click(x, y)
+                    press_key('tab', 1)
+                    bot.typewrite('01')
+                    press_key('tab', 2)
+                    press_key('space', 1)
+                else:
+                    bot.click(1112, 826)
+
+                    bot.click(1136, 1176)
+                    bot.sleep(0.3)
+                    press_key('enter', 1)
+                    bot.sleep(0.3)
+                    bot.typewrite(str(part_number))
+
+                    bot.sleep(0.3)
+                    bot.click(1136, 1176)
 
     line += 1
 
+bot.sleep(0.3)
 bot.click(1852, 1074)
