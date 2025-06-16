5f7c596920b2bce15ed148d094773814e411f002
JoviOakz <joao.mateussdc@gmail.com>
2025-06-16 14:30:51 -0300

    feat: OM apointment updated

--- 05 - WorkedHourPointing/02 - OM's/França.py
+++ 05 - WorkedHourPointing/02 - OM's/França.py
@@ -1,34 +1,82 @@
+# ===== LIBRARIES =====
+
+import win32com.client as win32
 import pyautogui as bot
-import pandas as pd
-import pyperclip
+
+# ===== GLOBAL SETTINGS =====
 
 bot.FAILSAFE = True
-bot.PAUSE = 1
+bot.PAUSE = 0.01
+
+# ===== INITIAL ACTION =====
 
 bot.click(1802, 14)
 
-excel_path = 'ApontamentoFrança.xlsx'
-df = pd.read_excel(excel_path, engine='openpyxl')
+# ===== EXCEL CONFIGURATION =====
 
-group = []
-line = 0
+excel = win32.gencache.EnsureDispatch('Excel.Application')
+wb = excel.Workbooks.Open(r"C:\Users\Mao8ct\Desktop\PyAutomateSAP\05 - WorkedHourPointing\02 - OM's\ApontamentoFrança.xlsx")
+ws = wb.Sheets('Sheet1')
 
-rwork = df.at[line, 'Trab Real']
-uni = df.at[line, 'Un']
-work_center = df.at[line, 'CenTrab']
-description = df.at[line, 'Descrição']
-center = df.at[line, 'Centro']
-tativ = df.at[line, 'Tativ']
-date = df.at[line, 'Data']
-edv = df.at[line, 'Nº Pessoal']
+# ===== FUNCTIONS =====
 
-for _ in range(15):
-    order = df.at[line, 'Ordem']
-    group.append(order)
-    line += 1
+def press_key(key, times):
+    for _ in range(times):
+        if key == 'shtab':
+            bot.hotkey('shift', 'tab')
+        else:
+            bot.press(key)
 
-all_values = '\n'.join(map(str, group))
+# ===== MAIN =====
 
-bot.press('tab')
-pyperclip.copy(all_values)
-bot.hotkey('ctrl', 'v')
+def main():
+    bot.sleep(0.5)
+    press_key('tab', 1)
+
+    ws.Range('A2:B23').Copy()
+    bot.hotkey('ctrl', 'v')
+    bot.sleep(0.5)
+
+    ws.Range('C2:E23').Copy()
+    press_key('tab', 3)
+    bot.hotkey('ctrl', 'v')
+    bot.sleep(0.5)
+
+    ws.Range('F2:J23').Copy()
+    press_key('tab', 5)
+    bot.hotkey('ctrl', 'v')
+    bot.sleep(0.5)
+    
+    press_key('shtab', 2)
+
+    for __ in range(21):
+        press_key('space', 1)
+        press_key('down', 1)
+    press_key('space', 1)
+
+    press_key('tab', 1)
+
+    for __ in range(21):
+        press_key('space', 1)
+        press_key('up', 1)
+    press_key('space', 1)
+
+    press_key('tab', 10)
+
+    for __ in range(21):
+        press_key('space', 1)
+        press_key('down', 1)
+    press_key('space', 1)
+
+if __name__ == '__main__':
+    main()
+
+
+
+# ===== CALCULO A SER FEITO =====
+
+# valor total mes / qtd total de ordens
+# 20.000 ÷ 22 = R$909,09 por OT
+
+# qtd em reais por ordem / qtd reais por hora
+# 909,09 ÷ 120 = 7,58 horas por OT
