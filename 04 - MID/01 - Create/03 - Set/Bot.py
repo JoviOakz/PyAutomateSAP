1fcfada02b7e9161aee3737317fe1e6fe10ab7dc
JoviOakz <joao.mateussdc@gmail.com>
2025-07-02 17:31:11 -0300

    feat: bot finished

--- 04 - MID/01 - Create/03 - Set/Bot.py
+++ 04 - MID/01 - Create/03 - Set/Bot.py
@@ -7,7 +7,7 @@ import pyperclip
 # ===== GLOBAL SETTINGS =====
 
 bot.FAILSAFE = True
-bot.PAUSE = 0.55
+bot.PAUSE = 0.3
 
 # ===== INITIAL ACTION =====
 
@@ -29,7 +29,7 @@ def press_key(key, times):
 
 def save():
     bot.click(1850, 946)
-    bot.sleep(6)
+    bot.sleep(3)
     bot.click(1136, 664)
 
 # ===== PROGRAM CONFIGURATION =====
@@ -47,95 +47,127 @@ def main():
         norm = df.at[line, 'Norma']
         qty = df.at[line, 'Quantidade']
         name = df.at[line, 'Nome']
+        life = df.at[line, 'Vida']
+        interval = df.at[line, 'Intervalo']
+        category = df.at[line, 'Categoria']
+        sector = df.at[line, 'Setor']
+        cc = df.at[line, 'Centro de custo']
+        serie = 2
 
         for __ in range(qty):
-            bot.click(1850, 946)
+            bot.click(1848, 942)
             bot.sleep(1.5)
 
-            bot.click(500, 390)
-            pyperclip.copy(name)
+            bot.click(532, 400)
+            bot.typewrite(name)
 
             if serie < 10:
-                press_key('ctrlv', 1)
-                bot.sleep(0.1)
                 bot.typewrite('0' + str(serie))
             else:
-                press_key('ctrlv', 1)
-                bot.sleep(0.1)
                 bot.typewrite(str(serie))
 
             press_key('tab', 2)
             bot.typewrite(str(norm))
-            bot.click(500, 520)
+            bot.click(536, 536)
             press_key('tab', 1)
 
             if serie < 10:
-                bot.typewrite('00' + str(serie))
-            else:
                 bot.typewrite('0' + str(serie))
+            else:
+                bot.typewrite(str(serie))
 
             press_key('tab', 1)
-            bot.typewrite('100000')
+            bot.typewrite(str(life))
             press_key('tab', 1)
-            bot.typewrite('15')
+            bot.typewrite(str(interval))
             press_key('tab', 1)
-            bot.moveTo(500, 580, 0.15)
-            bot.sleep(0.3)
-            bot.scroll(-1000)
-            bot.sleep(0.3)
-            bot.click()
+            bot.typewrite(str(category))
+            bot.click(560, 738)
             press_key('tab', 1)
-            bot.moveTo(920, 580, 0.15)
-            bot.sleep(0.3)
-            bot.scroll(-6000)
-            bot.sleep(0.3)
-            bot.click()
+            bot.typewrite(str(sector))
+            bot.click(918, 738)
             press_key('tab', 1)
-            bot.typewrite('12')
-            bot.sleep(0.3)
-
-            bot.click(1272, 724)
+            bot.typewrite(str(cc))
+            bot.click(1280, 738)
             press_key('tab', 2)
 
             if serie < 10:
-                bot.typewrite('00' + str(serie))
-            else:
                 bot.typewrite('0' + str(serie))
+            else:
+                bot.typewrite(str(serie))
 
-            bot.click(510, 950)
-            bot.sleep(0.3)
-            bot.click(1436, 1006)
-            bot.sleep(0.3)
-            bot.scroll(-6000)
+            bot.click(524, 958)
+            press_key('tab', 1)
+            press_key('space', 1)
+            press_key('tab', 2)
+
+            # =================================================
+
+            bot.moveTo(580, 760, 0.15)
             bot.sleep(0.3)
 
-            bot.click(500, 520)
-            bot.sleep(0.3)
-            bot.moveTo(582, 462, 0.15)
-            bot.sleep(0.3)
-            bot.scroll(-9735)
-            bot.sleep(0.3)
+            bot.scroll(-1676)
             bot.click()
-            bot.sleep(0.3)
-            bot.click(1436, 510)
-            bot.sleep(1)
 
-            for ___ in range(15):
-                bot.click(500, 520)
-                bot.sleep(0.5)
-                bot.moveTo(582, 462, 0.15)
-                bot.sleep(0.5)
-                bot.scroll(-58)
-                bot.sleep(0.5)
-                bot.click()
-                bot.sleep(0.5)
-                bot.click(1436, 510)
-                bot.sleep(1.1)
+            bot.press('tab')
+            bot.press('space')
+            bot.hotkey('shift', 'tab')
 
+            bot.scroll(-56)
+            bot.click()
+
+            bot.press('tab')
+            bot.press('space')
+            bot.hotkey('shift', 'tab')
+
+            bot.scroll(-56)
+            bot.click()
+
+            bot.press('tab')
+            bot.press('space')
+            bot.hotkey('shift', 'tab')
+
+            bot.scroll(-1500)
+            bot.click()
+
+            bot.press('tab')
+            bot.press('space')
+            bot.hotkey('shift', 'tab')
+
+            bot.scroll(-56)
+            bot.click()
+
+            bot.press('tab')
+            bot.press('space')
+            bot.hotkey('shift', 'tab')
+
+            bot.scroll(-56)
+            bot.click()
+
+            bot.press('tab')
+            bot.press('space')
+            bot.hotkey('shift', 'tab')
+
+            bot.scroll(-56)
+            bot.click()
+
+            bot.press('tab')
+            bot.press('space')
+            bot.hotkey('shift', 'tab')
+
+            bot.scroll(-56)
+            bot.click()
+
+            bot.press('tab')
+            bot.press('space')
+
+            # =================================================
+
+            bot.click(1660, 600)
             bot.scroll(-6000)
             bot.sleep(0.3)
-            bot.click(1400, 766)
-            bot.sleep(2)
+            bot.click(1412, 766)
+            bot.sleep(1)
 
             save()
             serie += 1
