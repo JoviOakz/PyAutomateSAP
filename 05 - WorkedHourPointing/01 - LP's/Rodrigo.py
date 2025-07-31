ee1a6bf38aa5d3d07f142d2369e83439b6ed4298
JoviOakz <joao.mateussdc@gmail.com>
2025-07-31 15:52:57 -0300

    feat (WorkedHourPointing): new code created for Rodrigo Melo

---
+++ 05 - WorkedHourPointing/01 - LP's/Rodrigo.py
@@ -0,0 +1,111 @@
+# ===== LIBRARIES =====
+
+import pyautogui as bot
+import pandas as pd
+
+# ===== GLOBAL SETTINGS =====
+
+bot.FAILSAFE = True
+bot.PAUSE = 0.75
+
+# ===== INITIAL ACTION =====
+
+bot.click(1802, 14)
+
+# ===== EXCEL CONFIGURATION =====
+
+EXCEL_PATH = 'ApontamentoRodrigo.xlsx'
+df = pd.read_excel(EXCEL_PATH, engine='openpyxl')
+
+# ===== FUNCTIONS =====
+
+def press_key(key, times):
+    for _ in range(times):
+        if key == 'ctrls':
+            bot.hotkey('ctrl', 's')
+        else:
+            bot.press(key)
+
+def open_diagram():
+    lp = df.at[line, 'LPs']
+    bot.typewrite(str(lp))
+    press_key('f7', 1)
+    bot.sleep(3)
+
+def verify_lp():
+    try:
+        not_exist_lp = list(bot.locateAllOnScreen('images/LPNOTEXIST.png', grayscale=True, confidence=0.9))
+
+        if not_exist_lp:
+            df.at[line, 'Status'] = 'LP doesn\'t exist!'
+            
+            return True
+
+    except Exception:
+        try:
+            exist_line = list(bot.locateAllOnScreen('images/NFILLEDLINE.png', grayscale=True, confidence=0.9))
+
+            if exist_line:
+                df.at[line, 'Status'] = 'Line created!'
+
+        except Exception:
+            df.at[line, 'Status'] = 'Line already used!'
+            press_key('f3', 2)
+
+            return True
+
+def create_apointment():
+    press_key('tab', 2)
+    bot.typewrite('Planejador Rodrigo - 31.07.2025')
+    press_key('tab', 2)
+    bot.typewrite('H')
+    press_key('tab', 2)
+    bot.typewrite('FCTMEO')
+    press_key('tab', 2)
+    bot.typewrite('100')
+    press_key('tab', 1)
+    bot.typewrite('025PROJ')
+    bot.sleep(0.3)
+
+def save_line():
+    press_key('ctrls', 1)
+    bot.sleep(2)
+    press_key('tab', 1)
+    press_key('enter', 1)
+    bot.sleep(1)
+    bot.typewrite('92866849')
+    press_key('enter', 1)
+    bot.sleep(1.25)
+    press_key('tab', 1)
+    bot.sleep(0.3)
+    press_key('enter', 1)
+    bot.sleep(3)
+
+# ===== PROGRAM CONFIGURATION =====
+
+lp_qty = 13
+line = 0
+repeat_qty = lp_qty - line
+
+# ===== MAIN =====
+
+def main():
+    global line
+
+    for _ in range(repeat_qty):
+        filled_line = False
+
+        open_diagram()
+        filled_line = verify_lp()
+
+        if not filled_line:
+            create_apointment()
+            save_line()
+
+        line += 1
+
+    df.to_excel(EXCEL_PATH, index=False, engine='openpyxl')
+    bot.alert(title='BotText', text='Programa encerrado!')
+
+if __name__ == '__main__':
+    main()
