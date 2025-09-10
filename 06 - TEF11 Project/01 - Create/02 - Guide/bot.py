f5a56eb105d8f89610cdf957cc0223db668b257b
JoviOakz <joao.mateussdc@gmail.com>
2025-09-10 10:08:22 -0300

    feat: updated code

--- 06 - TEF11 Project/01 - Create/02 - Guide/bot.py
+++ 06 - TEF11 Project/01 - Create/02 - Guide/bot.py
@@ -4,49 +4,69 @@ import pandas as pd
 
 # ----- Global Settings -----
 bot.FAILSAFE = True
-bot.PAUSE = 0.25
+bot.PAUSE = 0.125
 
 # ----- Initial Command -----
 bot.click(1804, 16)
-bot.sleep(1)
+bot.sleep(1.125)
 
 # ----- Functions -----
 def pressKey(key, count):
     for _ in range(count):
-        if key == 'crtlv':
-            bot.hotkey('ctrl', 'v')
+        if key == 'stab':
+            bot.hotkey('shift', 'tab')
         else:
             bot.press(key)
 
-def insertInfo():
-    bot.keyDown('ctrl')
-    bot.click(322, 180)
-    bot.keyUp('ctrl')
-    bot.sleep(1)
+def openTransaction():
+    bot.typewrite('CA01')
+    pressKey('enter', 1)
+    bot.sleep(1.375)
 
-    bot.typewrite('4700.100.980')
-    bot.sleep(0.25)
+def scriptCreation():
+    bot.typewrite('4700.100.981')
+    bot.sleep(0.625)
     pressKey('tab', 1)
-    bot.sleep(1)
-
+    bot.sleep(1.125)
     bot.typewrite('6854')
-    bot.sleep(0.25)
-    pressKey('tab', 5)
-    bot.sleep(0.15)
-    
-    bot.typewrite('MAO09092025')
-    bot.sleep(0.25)
+    bot.sleep(0.625)
     pressKey('tab', 1)
-    bot.sleep(0.15)
-    bot.typewrite('10.09.2025')
-    bot.sleep(0.25)
+    bot.sleep(1.125)
+    pressKey('tab', 4)
+    bot.sleep(0.275)
+    bot.typewrite('MAO10092025')
+    bot.sleep(0.375)
+    pressKey('tab', 1)
+    bot.sleep(0.275)
+    bot.typewrite('11.09.2025')
+    bot.sleep(0.375)
     pressKey('tab', 2)
-    bot.sleep(0.15)
-
+    bot.sleep(0.275)
     bot.typewrite('ZBR0001')
-    bot.sleep(0.25)
-    # pressKey('enter')
+    bot.sleep(0.375)
+
+    pressKey('enter', 1)
+    bot.sleep(0.875)
+    pressKey('enter', 1)
+    bot.sleep(0.875)
+
+    pressKey('tab', 7)
+    bot.sleep(0.375)
+    bot.typewrite('Q55')
+    bot.sleep(0.375)
+    pressKey('tab', 3)
+    bot.sleep(0.375)
+    bot.typewrite('1')
+    bot.sleep(0.375)
+    pressKey('stab', 17)
+    bot.sleep(0.375)
+    pressKey('enter', 1)
+
+def opInsertion():
+    print('Hello World!')
 
 # ----- Start Program -----
 if __name__ == '__main__':
-    insertInfo()
+    openTransaction()
+    scriptCreation()
+    opInsertion()
