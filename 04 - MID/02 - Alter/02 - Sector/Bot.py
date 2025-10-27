0b90c0e4a9ecfae3b205dcc430d97f97839b8f0b
JoviOakz <joao.mateussdc@gmail.com>
2025-10-27 17:28:19 -0300

    feat: new bot almost finished

--- 04 - MID/02 - Alter/02 - Sector/Bot.py
+++ 04 - MID/02 - Alter/02 - Sector/Bot.py
@@ -34,20 +34,63 @@ def find_palletDuBico():
     bot.sleep(0.75)
     press_key('space', 1)
     bot.sleep(0.75)
-    press_key('down', 37)
+    press_key('down', 34)
     bot.sleep(0.75)
     press_key('space', 1)
+    bot.sleep(0.75)
+    press_key('tab', 3)
+    bot.sleep(0.75)
+    press_key('space', 1)
+    bot.sleep(1)
 
 def change_to_impact():
-    print('a')
+    press_key('tab', 3)
+    bot.sleep(1)
+    press_key('space', 1)
+    bot.sleep(2)
+    press_key('tab', 25)
+    bot.sleep(0.75)
+    press_key('space', 1)
+    bot.sleep(0.75)
+    press_key('down', 38)
+    bot.sleep(0.75)
+    press_key('enter', 1)
+    bot.sleep(1)
+    press_key('enter', 1)
+    bot.sleep(1)
+    press_key('enter', 1)
+    bot.sleep(1)
+
+# def find_orange():
+#     screenshot = bot.screenshot()
+
+#     img = screenshot.convert('RGB')
+
+#     orange_color = (255, 150, 51)
+
+#     width, height = img.size
+
+#     for x in range(0, width, 5):
+#         for y in range(0, height, 5):
+#             r, g, b = img.getpixel((x, y))
+#             if (r, g, b) == orange_color:
+#                 return True
+            
+    return False
 
 # ===== MAIN =====
 
 def main():
-    find_palletDuBico()
-    # change_to_impact()
+    while True:
+        try:
+            if find_orange():
+                break
+            
+        except Exception:
+            find_palletDuBico()
+            change_to_impact()
 
-    # bot.alert(title='BotText', text='Programa encerrado!')
+    bot.alert(title='BotText', text='Programa encerrado!')
 
 if __name__ == '__main__':
     main()
