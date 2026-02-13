321242701baac7da56e5c53c88c3d754df5a2934
JoviOakz <joao.mateussdc@gmail.com>
2026-02-13 17:42:47 -0300

    feat: LP register bot updated

--- 05 - LP-Register/Bot.py
+++ 05 - LP-Register/Bot.py
@@ -2,6 +2,7 @@
 
 import pyautogui as bot
 import pandas as pd
+from datetime import date
 
 # ===== GLOBAL SETTINGS =====
 
@@ -25,6 +26,10 @@ def press_key(key, times):
             bot.hotkey('ctrl', 'f9')
         elif key == 'ctrla':
             bot.hotkey('ctrl', 'a')
+        elif key == 'stab':
+            bot.hotkey('shift', 'tab')
+        elif key == 'sf1':
+            bot.hotkey('shift', 'f1')
         else:
             bot.press(key)
 
@@ -40,13 +45,21 @@ def main():
     global line
 
     for _ in range(repeat_qty):
-        bot.typewrite('LP-055036')
+        bot.typewrite(df.at[line, 'Elemento PEP'])
         press_key('enter', 1)
         bot.sleep(1)
 
-        bot.PAUSE = 0.35
+        part_number = df.at[line, 'Part Number']
+        description = df.at[line, 'Denominação']
+        lp_title = f'{part_number} - {description}'
+        bot.typewrite(lp_title)
+
+        bot.PAUSE = 0.25
+        
         press_key('tab', 3)
-        bot.sleep(1)
+        bot.sleep(1.15)
+        bot.typewrite(lp_title)
+        bot.sleep(1.15)
         press_key('ctrlf9', 1)
         bot.sleep(1.5)
         press_key('down', 2)
@@ -56,10 +69,104 @@ def main():
         bot.PAUSE = 1.5
 
         press_key('ctrla', 1)
-        bot.typewrite('99999999')
+        bot.typewrite('68540028')
 
+        bot.PAUSE = 0.25
+
+        press_key('up', 3)
+        press_key('stab', 1)
+        press_key('right', 1)
+        press_key('enter', 1)
+        bot.sleep(1.15)
+
+        bot.PAUSE = 0.85
+
+        press_key('tab', 1)
+        bot.typewrite(date.today().strftime("%d.%m.%Y"))
+        press_key('down', 1)
+
+        # --
+        bot.typewrite(date.today().strftime("%d.%m.%Y"))
+        # --
+
+        # if data_atual > data_entrega:
+        #     bot.typewrite(date.today().strftime("%d.%m.%Y"))
+        # else:
+        #     bot.typewrite(data_entrega)
+
+        press_key('down', 1)
+        press_key('ctrla', 1)
+        bot.typewrite(date.today().strftime("%d.%m.%Y"))
+        press_key('down', 1)
+        press_key('ctrla', 1)
+
+        # --
+        bot.typewrite(date.today().strftime("%d.%m.%Y"))
+        # --
+
+        # if data_atual > data_entrega:
+        #     bot.typewrite(date.today().strftime("%d.%m.%Y"))
+        # else:
+        #     bot.typewrite(data_entrega)
+        
+        bot.PAUSE = 0.25
+
+        press_key('up', 3)
+        press_key('stab', 1)
+        press_key('right', 3)
+        press_key('enter', 1)
+        bot.sleep(1.15)
+        press_key('tab', 1)
+        press_key('down', 3)
+        bot.sleep(0.65)
+
+        bot.PAUSE = 0.85
+
+        bot.typewrite(df.at[line, 'Entregar para'])
+        bot.sleep(0.85)
+        press_key('tab', 2)
+        bot.typewrite(str(df.at[line, 'Quantidade']))
+        press_key('tab', 1)
+        bot.typewrite('PC')
+        press_key('tab', 1)
+        press_key('down', 1)
+        bot.typewrite(str(df.at[line, 'Custo estimado']))
+        press_key('tab', 1)
+        bot.typewrite('BRL')
+        press_key('enter', 1)
+        bot.sleep(1.25)
+
+        bot.PAUSE = 0.25
+
+        press_key('stab', 4)
+        press_key('enter', 1)
+        bot.sleep(1.15)
+
+        bot.PAUSE = 0.85
+
+        press_key('tab', 1)
+        
+        # --
+        bot.typewrite('ZPS007')
+        # --
+        
+        # calculo objeto de liquidação (ex: ZPS007)
+        
+        press_key('tab', 1)
+        bot.typewrite(str(df.at[line, 'Esquema de Alocação']))
+        press_key('f3', 1)
+        bot.sleep(0.65)
+        press_key('f3', 1)
+        bot.sleep(0.65)
+        press_key('sf1', 1)
+        press_key('down', 1)
+        press_key('tab', 1)
+        press_key('down', 1)
+        press_key('tab', 1)
+        bot.typewrite('68540028')
+        
         line += 1
 
 if __name__ == '__main__':
     main()
-    # bot.alert(title='BotText', text='Programa encerrado!')
+    bot.alert(title='BotText', text='Programa encerrado!')
