fbacb49ad65847854c48f0566a7501ae5f2eb5f6
JoviOakz <joao.mateussdc@gmail.com>
2026-04-29 13:46:08 -0300

    used: Rodrigo pointing

--- 04 - Pointing/01 - LP's/Rodrigo.py
+++ 04 - Pointing/01 - LP's/Rodrigo.py
@@ -103,7 +103,7 @@ def verify_lp():
 
 def create_apointment():
     press_key('tab', 2)
-    bot.typewrite('APS - Rodrigo - 30.03.2026')
+    bot.typewrite('APS - Rodrigo - 29.04.2026')
     press_key('tab', 2)
     bot.typewrite('H')
     press_key('tab', 2)
@@ -143,8 +143,8 @@ def save_line():
 
 # ===== PROGRAM CONFIGURATION =====
 
-lp_qty = 31
-line = 10
+lp_qty = 45
+line = 0
 repeat_qty = lp_qty - line
 
 # ===== MAIN =====
