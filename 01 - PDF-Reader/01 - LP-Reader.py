695bfbfb5adb713fd7692c374314939a2cd1698e
JoviOakz <joao.mateussdc@gmail.com>
2026-05-08 16:54:22 -0300

    fix

--- 01 - PDF-Reader/01 - LP-Reader.py
+++ 01 - PDF-Reader/01 - LP-Reader.py
@@ -8,7 +8,7 @@ from PIL import ImageEnhance, ImageFilter
 
 # ===== CONSTANTS =====
 
-KW = '03'
+KW = '05'
 ORIENTATION = 'deitado'  # [deitado] | [pé]
 ROTATION_ANGLE = 270  # [deitado -> 270] | [pé -> 0]
 
