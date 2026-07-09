49ab3fc1ee28a8192fabce5125de8b3b0473ec41
JoviOakz <joao.mateussdc@gmail.com>
2026-07-09 11:41:34 -0300

    feat: connection working

--- 00 - Tests/01 - ProgramTest/TestRedlakePy.py
+++ 00 - Tests/01 - ProgramTest/TestRedlakePy.py
@@ -1,24 +1,38 @@
 import oracledb
-import os
 
-os.environ['TNS_ADMIN'] = r'C:\Oracle\ora_net'
+try:
+    pasta_instant_client = r'C:\Oracle\instantclient_23_0' 
+    
+    oracledb.init_oracle_client(
+        lib_dir=pasta_instant_client,
+        config_dir=r'C:\Oracle\ora_net'
+    )
 
-alias = "REDLake_ZeusP_Consumer_Common.world"
+    print('🚀 Modo Thick ativado com sucesso!')
+
+except Exception as e:
+    print(f'❌ Falha ao iniciar cliente Oracle: {e}')
+
+usuario = 'MAO8CT'
+senha = '49l1)f=f3q6A'
+string_conexao_completa = '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=SI0EXARAC04.de.bosch.com)(PORT=38000))(CONNECT_DATA=(SERVICE_NAME=RLDP01_CON_4.BOSCH.COM)))'
 
 try:
     connection = oracledb.connect(
-        user="MAO8CT", 
-        password="49l1)f=f3q6A", 
-        dsn=alias
+        user=usuario,
+        password=senha,
+        dsn=string_conexao_completa
     )
-    print("Conexão realizada com sucesso!")
+
+    print('🔥 SUCESSO! Conectado com criptografia ativa ao banco da empresa!')
     
     cursor = connection.cursor()
-    cursor.execute("SELECT user FROM dual")
-    print(f"Conectado como: {cursor.fetchone()[0]}")
+    cursor.execute('SELECT user FROM dual')
+    print(f'Resultado do teste (Usuário Ativo): {cursor.fetchone()[0]}')
     
     cursor.close()
     connection.close()
+    print('Conexão encerrada com segurança.')
 
 except Exception as e:
-    print(f"Erro ao conectar: {e}")
+    print(f'❌ Erro na conexão: {e}')
