0b6088cc033ff4c1861b678af70bc54049313762
JoviOakz <joao.mateussdc@gmail.com>
2026-07-13 16:36:05 -0300

    feat: new data

--- 05 - LP-Register/Bot.py
+++ 05 - LP-Register/Bot.py
@@ -428,22 +428,22 @@ def diagram_creation():
 
 # ===== PROGRAM CONFIGURATION =====
 
-lp_qty = 58
+lp_qty = 55
 line = 0
 repeat_qty = lp_qty - line
 
 # ===== MAIN =====
 
 if __name__ == '__main__':
-    # wbs_element_creation()
+    wbs_element_creation()
 
-    line = 34
+    line = 0
     repeat_qty = lp_qty - line
 
     bot.PAUSE = 0.75
 
-    # enter_cn21()
-    # diagram_config()
+    enter_cn21()
+    diagram_config()
     diagram_creation()
 
     bot.alert(title='BotText', text='Program successfully completed')
