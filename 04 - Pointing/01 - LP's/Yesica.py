1d6cf3e48f51882cf9f45f337f77689479e43d93
JoviOakz <joao.mateussdc@gmail.com>
2026-07-23 13:39:36 -0300

    feat: data updated

--- 04 - Pointing/01 - LP's/Yesica.py
+++ 04 - Pointing/01 - LP's/Yesica.py
@@ -103,7 +103,7 @@ def verify_lp():
 
 def create_apointment():
     press_key('tab', 2)
-    bot.typewrite('APS - Yesica - 23.07.2026')
+    bot.typewrite('APS - Yesica - 24.07.2026')
     press_key('tab', 2)
     bot.typewrite('H')
     press_key('tab', 2)
@@ -143,8 +143,8 @@ def save_line():
 
 # ===== PROGRAM CONFIGURATION =====
 
-lp_qty = 121
-line = 109
+lp_qty = 64
+line = 0
 repeat_qty = lp_qty - line
 
 # ===== MAIN =====
