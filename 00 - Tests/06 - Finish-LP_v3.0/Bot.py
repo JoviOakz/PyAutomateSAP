853939fb4417f7ad17cde866e8d8c1d46e35291a
JoviOakz <joao.mateussdc@gmail.com>
2026-07-23 13:36:27 -0300

    feat: new projects

---
+++ 00 - Tests/06 - Finish-LP_v3.0/Bot.py
@@ -0,0 +1,138 @@
+# ===== LIBRARIES =====
+
+import pyautogui as bot
+import pandas as pd
+import pyperclip as pc
+from datetime import date
+import time
+import re
+
+# ===== GLOBAL SETTINGS =====
+
+bot.FAILSAFE = True
+bot.PAUSE = 0.75
+
+# ===== INITIAL ACTION =====
+
+bot.click(1802, 14)
+
+# ===== EXCEL CONFIGURATION =====
+
+EXCEL_PATH = '../98 - Excels/LP-Register.xlsx'
+df = pd.read_excel(EXCEL_PATH, engine='openpyxl')
+
+# ===== FUNCTIONS =====
+
+def press_key(key, times):
+    for _ in range(times):
+        if key == 'ctrla':
+            bot.hotkey('ctrl', 'a')
+        if key == 'ctrlc':
+            bot.hotkey('ctrl', 'c')
+        elif key == 'ctrls':
+            bot.hotkey('ctrl', 's')
+        elif key == 'ctrltab':
+            bot.hotkey('ctrl', 'tab')
+        elif key == 'ctrlstab':
+            bot.hotkey('ctrl', 'shift', 'tab')
+        else:
+            bot.press(key)
+
+def wait_event(img, region=None, timeout=10):
+    inicio = time.time()
+
+    while time.time() - inicio < timeout:
+        try:
+            local = bot.locateOnScreen(img, region=region, grayscale=True, confidence=0.9)
+
+            if local:
+                return local
+        except:
+            pass
+
+        time.sleep(0.5)
+    return None
+
+def open_lp():
+    if wait_event('images/PROJECT_BUILDER_1.png'):
+        pass
+    else:
+        bot.alert(title='Warning', text='Script error found!')
+        df.at[line, 'Status'] = 'Error'
+        df.to_excel(EXCEL_PATH, index=False, engine='openpyxl')
+        raise ValueError('\n\n------------- Error: -------------\n|> 1º Project Builder screen not found <|\n')
+    
+    press_key('alt', 1)
+    press_key('down', 2)
+    press_key('enter', 1)
+
+    if wait_event('images/PROJECT_BUILDER_2.png'):
+        pass
+    else:
+        bot.alert(title='Warning', text='Script error found!')
+        df.at[line, 'Status'] = 'Error'
+        df.to_excel(EXCEL_PATH, index=False, engine='openpyxl')
+        raise ValueError('\n\n------------- Error: -------------\n|> Open project window not found <|\n')
+
+    bot.typewrite(re.sub('-', '', str(df.at[line, 'LP'])))
+
+    press_key('enter', 1)
+
+def check_status():
+    if wait_event('images/PROJECT_BUILDER_3.png'):
+        pass
+    else:
+        bot.alert(title='Warning', text='Script error found!')
+        df.at[line, 'Status'] = 'Error'
+        df.to_excel(EXCEL_PATH, index=False, engine='openpyxl')
+        raise ValueError('\n\n------------- Error: -------------\n|> 2º Project Builder screen not found <|\n')
+
+    press_key('ctrltab', 4)
+    press_key('ctrla', 1)
+    press_key('ctrlc', 1)
+    press_key('enter', 1)
+
+    project_status = pc.paste()
+
+    if 'ABER' in project_status:
+        return 'ABER'
+    elif 'LIB' in project_status:
+        return 'LIB'
+    elif 'ENTE' in project_status:
+        return 'ENTE'
+    else:
+        return 'ENCE'
+
+def process_status(status):
+    match status:
+        case 'ABER':
+            print('1')
+        case 'ABER':
+            print('2')
+        case 'ABER':
+            print('3')
+        case 'ABER':
+            print('4')
+
+# ===== PROGRAM CONFIGURATION =====
+
+lp_qty = 1
+line = 0
+repeat_qty = lp_qty - line
+
+# ===== MAIN =====
+
+if __name__ == '__main__':
+    for _ in range(repeat_qty):
+        open_lp()
+        status = check_status()
+        process_status(status)
+        exist = diagram_verifier()
+
+        if exist:
+            close_diagram()
+
+        close_lp()
+        save()
+
+    bot.alert(title='BotText', text='Program successfully completed')
