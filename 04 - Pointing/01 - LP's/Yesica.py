01af49b052400a20e6f6815527a8d7cce2be9fbb
JoviOakz <joao.mateussdc@gmail.com>
2026-07-27 14:02:56 -0300

    feat: pointing Yesica bot updated

--- 04 - Pointing/01 - LP's/Yesica.py
+++ 04 - Pointing/01 - LP's/Yesica.py
@@ -3,11 +3,14 @@
 import pyautogui as bot
 import pyperclip as pc
 import pandas as pd
+import time
+import re
+from datetime import date
 
 # ===== GLOBAL SETTINGS =====
 
 bot.FAILSAFE = True
-bot.PAUSE = 1.25
+bot.PAUSE = 0.75
 
 # ===== INITIAL ACTION =====
 
@@ -22,88 +25,110 @@ df = pd.read_excel(EXCEL_PATH, engine='openpyxl')
 
 def press_key(key, times):
     for _ in range(times):
-        if key == 'ctrls':
-            bot.hotkey('ctrl', 's')
-        elif key == 'ctrlr':
-            bot.hotkey('ctrl', 'right')
-        elif key == 'ctrla':
+        if key == 'ctrla':
             bot.hotkey('ctrl', 'a')
         elif key == 'ctrlc':
             bot.hotkey('ctrl', 'c')
         elif key == 'stab':
             bot.hotkey('shift', 'tab')
+        elif key == 'ctrls':
+            bot.hotkey('ctrl', 's')
+        elif key == 'ctrltab':
+            bot.hotkey('ctrl', 'tab')
+        elif key == 'ctrlstab':
+            bot.hotkey('ctrl', 'shift', 'tab')
+        elif key == 'sspace':
+            bot.hotkey('shift', 'space')
         else:
             bot.press(key)
 
+def wait_event(img, region=None, timeout=10):
+    inicio = time.time()
+
+    while time.time() - inicio < timeout:
+        try:
+            local = bot.locateOnScreen(img, region=region, grayscale=True, confidence=0.9)
+
+            if local:
+                return local
+        except:
+            pass
+
+        time.sleep(0.5)
+    return None
+
+def apointment_verifier():
+    bot.PAUSE = 0.25
+    press_key('tab', 4)
+    press_key('ctrla', 1)
+    press_key('ctrlc', 1)
+    bot.PAUSE = 0.75
+
+    text = pc.paste().strip()
+
+    if text != 'H':
+        return False
+    else:
+        return True
+
+def wcenter_verifier():
+    bot.PAUSE = 0.25
+    press_key('tab', 2)
+
+    not_empty = True
+
+    while not_empty:
+        press_key('ctrla', 1)
+        press_key('ctrlc', 1)
+
+        wcenter = pc.paste().strip()
+
+        if wcenter != 'FF78012':
+            not_empty = False
+        else:
+            press_key('tab', 1)
+            press_key('ctrla', 1)
+            press_key('ctrlc', 1)
+            press_key('stab', 1)
+            press_key('down', 1)
+
+    press_key('stab', 4)
+    bot.PAUSE = 0.75
+
 def open_diagram():
-    lp = df.at[line, 'LPs']
+    if wait_event('images/DIAGRAM_1.png'):
+        pass
+    else:
+        bot.alert(title='Warning', text='Script error found!')
+        df.at[line, 'Status'] = 'Error'
+        df.to_excel(EXCEL_PATH, index=False, engine='openpyxl')
+        raise ValueError('\n\n------------- Error: -------------\n|> 1º Diagram screen not found <|\n')
+    
+    lp = re.sub('-', '', df.at[line, 'LPs'])
+    press_key('ctrla', 1)
     bot.typewrite(str(lp))
     press_key('f7', 1)
-    bot.sleep(3)
-
-def text_verifier():
-    text = pc.paste()
-    text = text.strip()
-
-    if text != 'x':
-        return True
-    else:
-        return False
 
 def verify_lp():
-    try:
-        not_exist_lp = list(bot.locateAllOnScreen('images/LPNOTEXIST.png', grayscale=True, confidence=0.9))
+    if wait_event('images/DIAGRAM_2.png'):
+        pass
+    else:
+        bot.alert(title='Warning', text='Script error found!')
+        df.at[line, 'Status'] = 'Error'
+        df.to_excel(EXCEL_PATH, index=False, engine='openpyxl')
+        raise ValueError('\n\n------------- Error: -------------\n|> 2º Diagram screen not found <|\n')
 
-        if not_exist_lp:
-            df.at[line, 'Status'] = 'LP doesn\'t exist!'
-            df.to_excel(EXCEL_PATH, index=False, engine='openpyxl')
-            bot.sleep(2)
-            
-            return True
+    have_apointment = apointment_verifier()
 
-    except Exception:
-        try:
-            h_exist = list(bot.locateAllOnScreen('images/H.png', grayscale=True, confidence=0.9))
-            
-            if h_exist:
-                df.at[line, 'Status'] = 'LP already have pointing!'
-                df.to_excel(EXCEL_PATH, index=False, engine='openpyxl')
-                bot.sleep(2)
-
-                press_key('f3', 2)
-
-                return True
-            
-        except Exception:
-            bot.PAUSE = 0.1
-            press_key('tab', 6)
-            bot.PAUSE = 0.65
-
-            result = True
-
-            while result:
-                press_key('ctrlr', 1)
-                press_key('backspace', 1)
-                bot.typewrite('x')
-                press_key('ctrla', 1)
-                press_key('ctrlc', 1)
-                press_key('backspace', 1)
-                press_key('tab', 1)
-                press_key('stab', 1)
-                press_key('down', 1)
-
-                result = text_verifier()
-
-            press_key('up', 1)
-            bot.PAUSE = 0.1
-            press_key('stab', 6)
-            bot.PAUSE = 1.25
-
-            return False
+    if not have_apointment:
+        wcenter_verifier()
+        return False
+    else:
+        press_key('f3', 2)
+        return True
 
 def create_apointment():
-    press_key('tab', 2)
-    bot.typewrite('APS - Yesica - 24.07.2026')
+    bot.typewrite('APS - Yesica - ' + date.today().strftime('%d.%m.%Y'))
     press_key('tab', 2)
     bot.typewrite('H')
     press_key('tab', 2)
@@ -116,56 +141,103 @@ def create_apointment():
 
 def save_line():
     press_key('ctrls', 1)
-    bot.sleep(2)
+
+    if wait_event('images/WARNING_1.png'):
+        pass
+    else:
+        bot.alert(title='Warning', text='Script error found!')
+        df.at[line, 'Status'] = 'Error'
+        df.to_excel(EXCEL_PATH, index=False, engine='openpyxl')
+        raise ValueError('\n\n------------- Error: -------------\n|> 1º Advertence window not found <|\n')
+    
     press_key('tab', 1)
     press_key('enter', 1)
-    bot.sleep(1)
+    bot.sleep(1.5)
     bot.typewrite('92886895')
     press_key('enter', 1)
-    bot.sleep(1.25)
+
+    if wait_event('images/WARNING_2.png'):
+        pass
+    else:
+        bot.alert(title='Warning', text='Script error found!')
+        df.at[line, 'Status'] = 'Error'
+        df.to_excel(EXCEL_PATH, index=False, engine='openpyxl')
+        raise ValueError('\n\n------------- Error: -------------\n|> 2º Advertence window not found <|\n')
+
     press_key('tab', 1)
-    bot.sleep(0.3)
+    bot.sleep(0.75)
     press_key('enter', 1)
-    bot.sleep(1.25)
 
-    try:
-        warning_exist = list(bot.locateAllOnScreen('images/WARNING.png', grayscale=True, confidence=0.9))
+    if wait_event('images/WARNING_3.png', timeout=2.5):
+        press_key('enter', 1)
+    else:
+        pass
 
-        if warning_exist:
-            press_key('enter', 1)
+def apoint_process():
+    if wait_event('images/DIAGRAM_2.png'):
+        pass
+    else:
+        bot.alert(title='Warning', text='Script error found!')
+        df.at[line, 'Status'] = 'Error'
+        df.to_excel(EXCEL_PATH, index=False, engine='openpyxl')
+        raise ValueError('\n\n------------- Error: -------------\n|> 2º Diagram screen not found <|\n')
 
-    except Exception as e:
-        print(f'Error: {e}')
+    press_key('sspace', 1)
+    bot.PAUSE = 0.25
+    press_key('ctrltab', 1)
+    press_key('tab', 5)
+    bot.PAUSE = 0.75
+    press_key('enter', 1)
+    bot.sleep(1)
+    press_key('down', 1)
+    bot.typewrite('92886895')
+    press_key('ctrltab', 1)
+    press_key('stab', 1)
+    bot.typewrite('APS - Yesica - ' + date.today().strftime('%d.%m.%Y'))
+    press_key('down', 1)
+    press_key('stab', 1)
+    press_key('space', 1)
+    press_key('down', 1)
+    press_key('space', 1)
+    press_key('ctrlstab', 2)
+    press_key('stab', 2)
+    press_key('up', 1)
+    bot.typewrite(str(df.at[line, 'Hour']).replace('.', ','))
+    press_key('enter', 1)
+    bot.sleep(0.75)
+    press_key('ctrlstab', 1)
+    press_key('enter', 1)
+    bot.sleep(0.5)
+    press_key('tab', 3)
+    bot.typewrite(str(df.at[line, 'Hour']).replace('.', ','))
 
+def save_apointment():
+    press_key('ctrls', 1)
     df.at[line, 'Status'] = 'Apointing created!'
     df.to_excel(EXCEL_PATH, index=False, engine='openpyxl')
-    bot.sleep(3)
 
 # ===== PROGRAM CONFIGURATION =====
 
-lp_qty = 64
-line = 0
+lp_qty = 36
+line = 24
 repeat_qty = lp_qty - line
 
 # ===== MAIN =====
 
-def main():
-    global line
-
+if __name__ == '__main__':
     for _ in range(repeat_qty):
-        filled_line = False
-
         open_diagram()
-        filled_line = verify_lp()
+        alr_ap = verify_lp()
 
-        if not filled_line:
+        if not alr_ap:
             create_apointment()
             save_line()
+
+            open_diagram()
+            apoint_process()
+            save_apointment()
 
         line += 1
 
     df.to_excel(EXCEL_PATH, index=False, engine='openpyxl')
-    bot.alert(title='BotText', text='Programa encerrado!')
-
-if __name__ == '__main__':
-    main()
+    bot.alert(title='BotText', text='Program successfully completed')
