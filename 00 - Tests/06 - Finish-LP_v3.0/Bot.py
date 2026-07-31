e7166d7c1aa09bf53d4c3e20a1dc00d7441b84d2
JoviOakz <joao.mateussdc@gmail.com>
2026-07-31 16:04:08 -0300

    update: finish LP v3.0 updated

--- 00 - Tests/06 - Finish-LP_v3.0/Bot.py
+++ 00 - Tests/06 - Finish-LP_v3.0/Bot.py
@@ -9,7 +9,7 @@ import time
 # ===== GLOBAL SETTINGS =====
 
 bot.FAILSAFE = True
-bot.PAUSE = 0.75
+bot.PAUSE = 0.85
 
 # ===== INITIAL ACTION =====
 
@@ -17,7 +17,7 @@ bot.click(1802, 14)
 
 # ===== EXCEL CONFIGURATION =====
 
-EXCEL_PATH = '../98 - Excels/LP-Register.xlsx'
+EXCEL_PATH = '../../01 - Excels/TEF3.xlsx'
 df = pd.read_excel(EXCEL_PATH, engine='openpyxl')
 
 # ===== FUNCTIONS =====
@@ -30,12 +30,18 @@ def press_key(key, times):
             bot.hotkey('ctrl', 'c')
         elif key == 'ctrls':
             bot.hotkey('ctrl', 's')
+        elif key == 'stab':
+            bot.hotkey('shift', 'tab')
+        elif key == 'sspace':
+            bot.hotkey('shift', 'space')
         elif key == 'ctrle':
             bot.hotkey('ctrl', 'enter')
         elif key == 'ctrltab':
             bot.hotkey('ctrl', 'tab')
         elif key == 'ctrlstab':
             bot.hotkey('ctrl', 'shift', 'tab')
+        elif key == 'alte':
+            bot.hotkey('alt', 'e')
         else:
             bot.press(key)
 
@@ -62,10 +68,10 @@ def open_lp():
         df.at[line, 'Status'] = 'Error'
         df.to_excel(EXCEL_PATH, index=False, engine='openpyxl')
         raise ValueError('\n\n------------- Error: -------------\n|> 1º Project Builder screen not found <|\n')
-    
+
+    press_key('stab', 1)
     press_key('alt', 1)
-    press_key('down', 2)
-    press_key('enter', 1)
+    press_key('b', 1)
 
     if wait_event('images/PROJECT_BUILDER_2.png'):
         pass
@@ -78,51 +84,6 @@ def open_lp():
     bot.typewrite(str(df.at[line, 'LP']))
     press_key('enter', 1)
 
-def close_project():
-    press_key('alt', 1)
-    press_key('right', 1)
-    press_key('down', 2)
-    press_key('right', 1)
-    press_key('down', 4)
-    press_key('right', 1)
-    press_key('enter', 1)
-    bot.sleep(0.75)
-
-    press_key('alt', 1)
-    press_key('right', 1)
-    press_key('down', 2)
-    press_key('right', 1)
-    press_key('down', 6)
-    press_key('right', 1)
-    press_key('enter', 1)
-    bot.sleep(0.75)
-
-    press_key('ctrls', 1)
-
-def close_diagram():
-    press_key('alt', 1)
-    press_key('right', 1)
-    press_key('down', 2)
-    press_key('right', 1)
-    press_key('down', 4)
-    press_key('right', 1)
-    press_key('enter', 1)
-    bot.sleep(0.75)
-
-    press_key('alt', 1)
-    press_key('right', 1)
-    press_key('down', 2)
-    press_key('right', 1)
-    press_key('down', 6)
-    press_key('right', 1)
-    press_key('enter', 1)
-    bot.sleep(0.75)
-
-    press_key('ctrlstab', 3)
-    press_key('up', 2)
-    press_key('ctrle', 1)
-    bot.sleep(0.75)
-
 def check_status():
     if wait_event('images/PROJECT_BUILDER_3.png'):
         pass
@@ -132,17 +93,20 @@ def check_status():
         df.to_excel(EXCEL_PATH, index=False, engine='openpyxl')
         raise ValueError('\n\n------------- Error: -------------\n|> 2º Project Builder screen not found <|\n')
 
+    bot.PAUSE = 0.25
     press_key('ctrltab', 4)
     press_key('ctrla', 1)
     press_key('ctrlc', 1)
     press_key('enter', 1)
+    bot.PAUSE = 0.85
+    bot.sleep(0.85)
 
     project_status = pc.paste()
 
-    # Inserir o Status -> LBPA
-
     if 'ABER' in project_status:
         return 'ABER'
+    elif 'LBPA' in project_status:
+        return 'LBPA'
     elif 'LIB' in project_status:
         return 'LIB'
     elif 'ENTE' in project_status:
@@ -154,35 +118,265 @@ def verify_diagram():
     press_key('ctrlstab', 3)
     press_key('down', 1)
     press_key('right', 1)
-    press_key('ctrlstab', 3)
-    press_key('up', 1)
-    press_key('down', 2)
+    bot.sleep(1.25)
+    press_key('down', 1)
     press_key('ctrle', 1)
-    bot.sleep(0.75)
-
+    bot.sleep(1.25)
     press_key('ctrltab', 1)
     press_key('ctrlstab', 1)
     press_key('ctrla', 1)
     press_key('ctrlc', 1)
 
-    lp = pc.paste()
+    diagram = pc.paste().strip()
 
-    if '-' in lp:
-        print('a')
+    if '-' not in diagram:
+        return True
+    else:
+        return False
+
+def close_diagram():
+    press_key('ctrltab', 2)
+    press_key('enter', 1)
+    bot.sleep(1.25)
+    press_key('tab', 6)
+
+    content = True
+    total_lines = 0
+    diagram_line = 0
+    repeat_process = total_lines - diagram_line
+
+    while content:
+        press_key('ctrla', 1)
+        press_key('ctrlc', 1)
+
+        wcenter = pc.paste().strip()
+
+        if wcenter == 'FF78012':
+            pass
+        elif 'FCT' in wcenter:
+            diagram_line += 1
+        else:
+            content = False
+
+        press_key('tab', 1)
+        press_key('ctrlc', 1)
+        press_key('stab', 1)
+        press_key('down', 1)
+        total_lines += 1
+
+    press_key('up', repeat_process)
+
+    for _ in range(repeat_process):
+        press_key('sspace', 1)
+        press_key('down', 1)
+
+    press_key('ctrltab', 1)
+    press_key('tab', 5)
+    press_key('enter', 1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+    press_key('alte', 1)
+    press_key('s', 1)
+    press_key('t', 1)
+    press_key('d', 1)
+    bot.sleep(0.85)
+
+    press_key('alte', 1)
+    press_key('s', 1)
+    press_key('a', 1)
+    press_key('d', 1)
+    bot.sleep(0.85)
+
+    if wait_event('images/WARNING_1.png', timeout=2.25):
+        press_key('tab', 1)
+        press_key('enter', 1)
+        df.at[line, 'Status'] = 'Apropriar na CJ88'
+        df.to_excel(EXCEL_PATH, index=False, engine='openpyxl')
+    else:
+        df.at[line, 'Status'] = 'Encerrado'
+        df.to_excel(EXCEL_PATH, index=False, engine='openpyxl')
+
+    bot.sleep(0.85)
+    press_key('ctrls', 1)
+
+def close_project():
+    press_key('alte', 1)
+    press_key('s', 1)
+    press_key('t', 1)
+    press_key('d', 1)
+    bot.sleep(0.85)
+
+    press_key('alte', 1)
+    press_key('s', 1)
+    press_key('a', 1)
+    press_key('d', 1)
+    bot.sleep(0.85)
+
+    if wait_event('images/WARNING_1.png', timeout=2.25):
+        press_key('tab', 1)
+        press_key('enter', 1)
+        df.at[line, 'Status'] = 'Apropriar na CJ88'
+        df.to_excel(EXCEL_PATH, index=False, engine='openpyxl')
+    else:
+        df.at[line, 'Status'] = 'Encerrado'
+        df.to_excel(EXCEL_PATH, index=False, engine='openpyxl')
+
+    bot.sleep(0.85)
+    press_key('ctrls', 1)
 
 def process_lp(status):
     match status:
         case 'ABER':
             close_project()
 
+        # case 'LBPA':
+        #     close_project()
+
         case 'LIB':
-            verify_diagram()
+            diagram_exist = verify_diagram()
+
+            if diagram_exist:
+                close_diagram()
+                close_project()
 
         case 'ENTE':
-            print('3')
+            press_key('f3', 1)
+            df.at[line, 'Status'] = 'Apropriar na CJ88'
+            df.to_excel(EXCEL_PATH, index=False, engine='openpyxl')
 
         case 'ENCE':
-            print('4')
+            press_key('f3', 1)
+            df.at[line, 'Status'] = 'Encerrado'
+            df.to_excel(EXCEL_PATH, index=False, engine='openpyxl')
+
+def cj88_config():
+    if wait_event('images/PROJECT_BUILDER_1.png'):
+        pass
+    else:
+        bot.alert(title='Warning', text='Script error found!')
+        df.at[line, 'Status'] = 'Error'
+        df.to_excel(EXCEL_PATH, index=False, engine='openpyxl')
+        raise ValueError('\n\n------------- Error: -------------\n|> 1º Project Builder screen not found <|\n')
+    
+    bot.typewrite('/NCJ88')
+    press_key('enter', 1)
+
+    if wait_event('images/ACC.png'):
+        pass
+    else:
+        bot.alert(title='Warning', text='Script error found!')
+        df.at[line, 'Status'] = 'Error'
+        df.to_excel(EXCEL_PATH, index=False, engine='openpyxl')
+        raise ValueError('\n\n------------- Error: -------------\n|> 1º Project Builder screen not found <|\n')
+
+    bot.typewrite('0010')
+    press_key('enter', 1)
+
+    if wait_event('images/APPROPRIATION_1.png'):
+        pass
+    else:
+        bot.alert(title='Warning', text='Script error found!')
+        df.at[line, 'Status'] = 'Error'
+        df.to_excel(EXCEL_PATH, index=False, engine='openpyxl')
+        raise ValueError('\n\n------------- Error: -------------\n|> 1º Appropriation screen not found <|\n')
+
+    bot.typewrite(str(df.at[line, 'LP']))
+    press_key('tab', 2)
+    press_key('down', 1)
+    press_key('space', 1)
+    press_key('down', 1)
+    press_key('space', 1)
+    press_key('tab', 1)
+    bot.typewrite('7')
+    press_key('tab', 1)
+    bot.typewrite('7')
+    press_key('tab', 1)
+    bot.typewrite('2026')
+    press_key('tab', 1)
+    bot.typewrite(date.today().strftime('%d.%m.%Y'))
+    press_key('enter', 1)
+
+def appropriate_lp():
+    if wait_event('images/APPROPRIATION_1.png'):
+        pass
+    else:
+        bot.alert(title='Warning', text='Script error found!')
+        df.at[line, 'Status'] = 'Error'
+        df.to_excel(EXCEL_PATH, index=False, engine='openpyxl')
+        raise ValueError('\n\n------------- Error: -------------\n|> 1º Appropriation screen not found <|\n')
+    
+    bot.typewrite(str(df.at[line, 'LP']))
+    press_key('ctrltab', 2)
+    press_key('space', 1)
+    press_key('f8', 1)
+
+    if wait_event('images/WARNING_2.png', timeout=2.25):
+        df.at[line, 'Status'] = 'Apropriado'
+        df.to_excel(EXCEL_PATH, index=False, engine='openpyxl')
+    else:
+        if wait_event('images/APPROPRIATION_2.png'):
+            df.at[line, 'Status'] = 'Apropriado'
+            df.to_excel(EXCEL_PATH, index=False, engine='openpyxl')
+            press_key('f3')
+        else:
+            bot.alert(title='Warning', text='Script error found!')
+            df.at[line, 'Status'] = 'Error'
+            df.to_excel(EXCEL_PATH, index=False, engine='openpyxl')
+            raise ValueError('\n\n------------- Error: -------------\n|> 1º Appropriation screen not found <|\n')
+
+def cj20n_config():
+    if wait_event('images/APPROPRIATION_1.png'):
+        pass
+    else:
+        bot.alert(title='Warning', text='Script error found!')
+        df.at[line, 'Status'] = 'Error'
+        df.to_excel(EXCEL_PATH, index=False, engine='openpyxl')
+        raise ValueError('\n\n------------- Error: -------------\n|> 1º Appropriation screen not found <|\n')
+
+    press_key('ctrlstab', 1)
+    press_key('tab', 1)
+    bot.typewrite('/NCJ20N')
+    press_key('enter', 1)
+
+def ence_project():
+    press_key('ctrltab', 1)
+    press_key('alte', 1)
+    press_key('s', 1)
+    press_key('a', 1)
+    press_key('d', 1)
+    bot.sleep(0.85)
+
+    if wait_event('images/WARNING_1.png', timeout=2.25):
+        press_key('tab', 1)
+        press_key('enter', 1)
+        df.at[line, 'Status'] = 'Error'
+        df.to_excel(EXCEL_PATH, index=False, engine='openpyxl')
+    else:
+        df.at[line, 'Status'] = 'Encerrado'
+        df.to_excel(EXCEL_PATH, index=False, engine='openpyxl')
+
+    press_key('ctrls', 1)
 
 # ===== PROGRAM CONFIGURATION =====
 
@@ -197,5 +391,29 @@ if __name__ == '__main__':
         open_lp()
         status = check_status()
         process_lp(status)
+        line += 1
+
+    line = 0
+    cj88_config()
+
+    for _ in range(lp_qty):
+        lp_status = str(df.at[line, 'Status'])
+
+        if lp_status == 'Apropriar na CJ88':
+            appropriate_lp()
+
+        line += 1
+
+    line = 0
+    cj20n_config()
+
+    for _ in range(lp_qty):
+        lp_status = str(df.at[line, 'Status'])
+
+        if lp_status == 'Apropriado':
+            open_lp()
+            ence_project()
+
+        line += 1
 
     bot.alert(title='BotText', text='Program successfully completed')
